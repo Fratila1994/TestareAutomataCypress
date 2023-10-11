dfa3dce179d154d389c608cc102f423dec32522d
Fratila Ionut <145117752+Fratila1994@users.noreply.github.com>
2023-10-11 22:12:38 +0300

    Add files via upload

---
+++ cypress/e2e/TesteLinkedn/TesteLinkedn.cy.js
@@ -0,0 +1,11 @@
+//describe('On Linkedin', () => {
+//it('I can login', () => {
+    //cy.visit('https://linkedin.com');
+   // cy.get('.nav__button-secondary').click();
+    //cy.get('#username').type('fratilaionutmadalin@gmail.com');
+   // cy.get('#password').type('Muiesteaua1923!');
+    //cy.get('.btn__primary--large').click();
+//})
+
+
+//})
